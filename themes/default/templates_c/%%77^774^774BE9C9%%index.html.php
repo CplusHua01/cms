a6bc837ad13428a10e0a13f1d9e5b38a34a11898
iiusky <iiusky@126.com>
2015-03-31 15:47:15 +0800

    增加jquery的map文件 修改登录后的页面

---
+++ themes/default/templates_c/%%77^774^774BE9C9%%index.html.php
@@ -0,0 +1,20 @@
+<?php /* Smarty version 2.6.25, created on 2015-03-30 15:19:45
+         compiled from index.html */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title><?php echo $this->_tpl_vars['show']['sitename']; ?>
+ | <?php echo $this->_tpl_vars['show']['sitedesc']; ?>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="<?php echo $this->_tpl_vars['show']['keywords']; ?>
+">
+<meta name="description" content="<?php echo $this->_tpl_vars['show']['description']; ?>
+">
+<script>if(top.location!==self.location) top.location=self.location;</script>
+</head>
+<body>
+Hello,world~_^
+</body>
+</html>
